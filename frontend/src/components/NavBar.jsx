f855b601342f179ca05a3c7b1ae16d352708f2fc
mateormz <mateo.ramirez@utec.edu.pe>
2024-11-30 12:29:43 -0500

    fix NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <nav className="bg-white p-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-white text-lg font-bold hover:text-gray-200">
+                <Link to="/home" className="text-black text-lg font-bold">
                     Hotel App
                 </Link>
                 <ul className="flex space-x-4">
@@ -15,23 +15,23 @@ const NavBar = () => {
                     {!tenantId ? (
                         <>
                             <li>
-                                <Link to="/login" className="text-white hover:text-gray-200">Login</Link>
+                                <Link to="/login" className="text-black hover:text-gray-500">Login</Link>
                             </li>
                             <li>
-                                <Link to="/register" className="text-white hover:text-gray-200">Register</Link>
+                                <Link to="/register" className="text-black hover:text-gray-500">Register</Link>
                             </li>
                         </>
                     ) : (
                         <>
                             {/* Si el usuario está autenticado, mostrar Profile, Reservas y Pagos */}
                             <li>
-                                <Link to="/profile" className="text-white hover:text-gray-200">Profile</Link>
+                                <Link to="/profile" className="text-black hover:text-gray-500">Profile</Link>
                             </li>
                             <li>
-                                <Link to="/reservations" className="text-white hover:text-gray-200">Reservas</Link>
+                                <Link to="/reservations" className="text-black hover:text-gray-500">Reservas</Link>
                             </li>
                             <li>
-                                <Link to="/payments" className="text-white hover:text-gray-200">Pagos</Link>
+                                <Link to="/payments" className="text-black hover:text-gray-500">Pagos</Link>
                             </li>
                         </>
                     )}
