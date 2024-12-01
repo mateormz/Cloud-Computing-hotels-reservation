f431148f02614c5f389a68b80406fcdb2c0f67b9
mateormz <mateo.ramirez@utec.edu.pe>
2024-12-01 12:12:13 -0500

    env serverless & deploy stages

---
+++ backend/deploy-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Definir el directorio principal donde están las APIs
+base_dir=$(pwd)
+
+# Encontrar todos los subdirectorios que contienen un archivo 'serverless.yml'
+api_dirs=()
+for dir in */; do
+  if [ -f "$dir/serverless.yml" ]; then
+    api_dirs+=("$dir")
+  fi
+done
+
+# Función para hacer deploy y ejecutar sls info
+deploy_api() {
+  api_dir=$1
+  echo "Desplegando API en el directorio: $api_dir"
+  
+  # Realizar el despliegue en el entorno 'test'
+  cd "$base_dir/$api_dir"
+  sls deploy --stage test
+  
+  # Ejecutar sls info para verificar el despliegue
+  sls info --stage test
+  
+  # Volver al directorio original
+  cd "$base_dir"
+}
+
+# Desplegar todas las APIs
+for api in "${api_dirs[@]}"; do
+  deploy_api "$api"
+done
